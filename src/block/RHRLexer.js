5bddd86fa0998bb4ed39320822ab5bd236729961
Michael Albinson <mikealbinson@gmail.com>
2020-08-23 16:42:37 -0400

    🛠: Fixes to the HRLexer to conform to specifications
    More tests pulled from the specs

--- src/block/RHRLexer.js
+++ src/block/RHRLexer.js
@@ -8,9 +8,30 @@ const ARLexer = require('../ARLexer');
  */
 class RHRLexer extends ARLexer {
 	static apply(text) {
-		text = text.replace(/^[ ]{0,3}[*]{3,}[ ]*$/gim, '<hr />');
-		text = text.replace(/^[ ]{0,3}[_]{3,}[ ]*$/gim, '<hr />');
-		return text.replace(/^[ ]{0,3}[-]{3,}[ ]*$/gim, '<hr />');
+		text = text.replace(/^[ ]{0,3}\*[* ]{2,}[ \t]*$/gim, RHRLexer._asteriskMatcher);
+		text = text.replace(/^[ ]{0,3}_[_ ]{2,}[ \t]*$/gim, RHRLexer._underscoreMatcher);
+		return text.replace(/^[ ]{0,3}-[- ]{2,}[ \t]*$/gim, RHRLexer._dashMatcher);
+	}
+
+	static _asteriskMatcher(match) {
+		return RHRLexer._min3Matches(/\*/g, match);
+	}
+
+	static _underscoreMatcher(match) {
+		return RHRLexer._min3Matches(/_/g, match);
+	}
+
+	static _dashMatcher(match) {
+		return RHRLexer._min3Matches(/-/g, match);
+	}
+
+	static _min3Matches(regExp, match) {
+		// there has to be at least 3 instances of the marker token in the string to qualify as a thematic break
+		const countMatches = match.match(regExp);
+		if (countMatches && countMatches.length >= 3)
+			return '<hr />';
+
+		return match;
 	}
 }
 
--- test/block/RHRLexer.test.js
+++ test/block/RHRLexer.test.js
@@ -5,6 +5,10 @@ const { expect } = require('chai');
 const RHRLexer = require('../../src/block/RHRLexer');
 
 
+/**
+ * Applies the test cases outlined in the CommonMark Specification section 4.1 (examples 13-31) as well as assorted
+ * smoke test cases
+ */
 describe('RHRLexer', () => {
 	describe('#apply', () => {
 		const assertOutput = (input, expected) => {
@@ -48,6 +52,44 @@ describe('RHRLexer', () => {
 			assertOutput(input, expected);
 		});
 
+		it('should follow the CommonMark specification examples', () => {
+			// example 13
+			assertOutput('***', '<hr />');
+			assertOutput('---', '<hr />');
+			assertOutput('___', '<hr />');
+
+			// example 14
+			assertOutput('+++', '+++');
+
+			//example 15
+			assertOutput('===', '===');
+
+			// example 16
+			assertOutput('--', '--');
+			assertOutput('**', '**');
+			assertOutput('__', '__');
+
+			// example 17
+			assertOutput(' ***', '<hr />');
+			assertOutput('  ***', '<hr />');
+			assertOutput('   ***', '<hr />');
+
+			// example 18
+			assertOutput('    ***', '    ***');
+
+			// custom example inspired by example 18
+			assertOutput('\t***', '\t***');
+
+			// example 19
+			assertOutput('Foo\n' +
+				'    ***', 'Foo\n' +
+				'    ***');
+
+
+			// example 20
+			assertOutput('_____________________________________', '<hr />');
+		});
+
 		it('should allow spaces', () => {
 			assertOutput(' - ---- ------ --- -', '<hr />');
 			assertOutput(' *  *** ** **** ', '<hr />');
@@ -76,6 +118,41 @@ describe('RHRLexer', () => {
 		it('should ignore non-matched whitespace characters', () => {
 			// example 26
 			assertOutput(' *-*', ' *-*');
-		})
+		});
+
+		it('should follow examples 27-31', () => {
+			// example 27
+			assertOutput(
+				'- foo\n' +
+				'***\n' +
+				'- bar',
+				'- foo\n' +
+				'<hr />\n' +
+				'- bar')
+
+			// example 28
+			assertOutput('Foo\n' +
+				'***\n' +
+				'bar',
+				'Foo\n' +
+				'<hr />\n' +
+				'bar');
+
+			// example 29 omitted as it shows the precedence of the heading lexer over the hr lexer... nothing to see in unit testing
+
+			// example 30
+			assertOutput('* Foo\n' +
+				'* * *\n' +
+				'* Bar',
+				'* Foo\n' +
+				'<hr />\n' +
+				'* Bar');
+
+			// example 31 modified as it also demonstrates the re-lex cycle as part of list parsing
+			assertOutput('- Foo\n' +
+				'- * * *',
+				'- Foo\n' +
+				'- * * *');
+		});
 	});
 });
